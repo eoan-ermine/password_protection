d1c648b49ac84393e0954f237e97f125d4435f01
Danil Sidoruk <danilsidoruknv@yandex.ru>
2023-11-16 22:29:29 +0300

    Implement createUser databaseManager operation

--- pass_protection/database_manager.cpp
+++ pass_protection/database_manager.cpp
@@ -1,6 +1,9 @@
 #include "database_manager.hpp"
 
 #include <QDebug>
+#include <QSqlError>
+#include <QSqlQuery>
+#include <qurl.h>
 
 DatabaseManager::DatabaseManager(const QString &path) {
   database = QSqlDatabase::addDatabase("QSQLITE");
@@ -12,3 +15,54 @@ DatabaseManager::DatabaseManager(const QString &path) {
     qDebug() << "Database: connection ok";
   }
 }
+
+bool DatabaseManager::createUser(
+    const QString &username, const QString &pass, const QString &lastName,
+    const QString &firstName, const QString &patronymic, const QDate &birthDate,
+    const QString &birthPlace, const QString &phone) {
+  bool success = false;
+
+  QSqlQuery selectQ;
+  selectQ.prepare(
+      R"(SELECT "username" FROM "main"."users" WHERE username = ?)");
+  selectQ.addBindValue(username);
+  selectQ.exec();
+  if (!selectQ.exec()) {
+    qDebug() << "selectUser error:" << selectQ.lastError();
+    return false;
+  } else {
+    qDebug() << "selectUser status:" << "ok";
+    if (selectQ.next()) {
+      qDebug() << "selectUser collision";
+      return false;
+    }
+  }
+
+  QSqlQuery insertQ;
+  insertQ.prepare(
+      R"(INSERT INTO "main"."users" ("username", "password", "last_name", "first_name", "patronymic", "birthdate", "birthplace", "phone") VALUES (?, ?, ?, ?, ?, ?, ?, ?);)");
+
+  insertQ.addBindValue(username);
+  insertQ.addBindValue(pass);
+  insertQ.addBindValue(lastName);
+  insertQ.addBindValue(firstName);
+  insertQ.addBindValue(patronymic);
+  insertQ.addBindValue(birthDate.toString());
+  insertQ.addBindValue(birthPlace);
+  insertQ.addBindValue(phone);
+
+  if (insertQ.exec()) {
+    success = true;
+  } else {
+    qDebug() << "createUser error:" << insertQ.lastError();
+  }
+
+  return success;
+}
+
+bool DatabaseManager::changePass(
+    const QString &username, const QString &pass, const QString &lastName,
+    const QString &firstName, const QString &patronymic, const QDate &birthDate,
+    const QString &birthPlace, const QString &phone) {}
+
+bool DatabaseManager::login(const QString &username, const QString &pass) {}
--- pass_protection/database_manager.hpp
+++ pass_protection/database_manager.hpp
@@ -1,9 +1,22 @@
+#include <QDate>
 #include <QSqlDatabase>
 #include <QString>
 
 class DatabaseManager {
 public:
   DatabaseManager(const QString &path);
+
+  bool createUser(const QString &username, const QString &pass,
+                  const QString &lastName, const QString &firstName,
+                  const QString &patronymic, const QDate &birthDate,
+                  const QString &birthPlace, const QString &phone);
+
+  bool changePass(const QString &username, const QString &pass,
+                  const QString &lastName, const QString &firstName,
+                  const QString &patronymic, const QDate &birthDate,
+                  const QString &birthPlace, const QString &phone);
+
+  bool login(const QString &username, const QString &pass);
 
 private:
   QSqlDatabase database;
